304bedddb7118068dd8773f2691485545b15c6c9
kotani <70792620+kotani@users.noreply.github.com>
2025-04-12 22:15:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
-
-# モデルを読み込む
+# model
 with open("SVM.pkl", "rb") as f:
     model = pickle.load(f) 
 
